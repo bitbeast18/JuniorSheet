da24df7e67fb08caffc17a344d870846007fda95
Harshal Nandigramwar <harshal.11711777@lpu.in>
2020-07-09 23:43:07 +0530

    Adds a template in every folder

---
+++ CF-D3/template.cpp
@@ -0,0 +1,29 @@
+/*
+*   1. What was the key concept?
+*   2. What mistake did I make? (if any)
+*   3. Thing to remember from now on.
+*/
+
+#include <iostream>
+using namespace std;
+
+#define INF 0x3f3f3f3f;
+#define INFL 0x3f3f3f3f3f3f3f3f;
+#define NITRO ios_base::sync_with_stdio(false); cin.tie(false)
+#define PRECISE cout << fixed << setprecision(9)
+
+using ll = long long int;
+using ld = long double;
+
+const int MOD = 1e9 + 7;
+const int N = 1e6 + 5;
+
+int main(){
+    // code here.
+    cout << INFL;
+    return 0;
+}
+
+// Check the corner cases. (n = 1).
+// Check overflows. (int vs ll).
+// Be consistent.
